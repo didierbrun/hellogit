b2eb7364d266dfcd5ac323928a4f3e55298a94bc
Didier Brun <db@big-boss-studio.com>
2021-12-14 21:34:55 +0100

    Fix merge

--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
 name = input("Quel est ton nom ?")
-<<<<<<< HEAD
 firstname = input("Quel est ton prénom ?")
 print("Hello", firstname, name)
-=======
 print("Hello ", name)
 
 print("Joyeux noël !")
->>>>>>> test/noel
